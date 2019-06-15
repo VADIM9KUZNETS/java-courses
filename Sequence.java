21dd789bb089c22b82f4b94a84733270602f6edd
login <9moyapochta9@gmail.com>
2019-06-15 20:46:52 +0300

    add sequence

---
+++ Sequence.java
@@ -0,0 +1,9 @@
+public class Sequence {
+    public static void main(String[] arg) {
+int a = 0;
+        for (int i = 1; i <= 14; i++) {
+            a += 7;
+            System.out.print(a + " ");
+        }
+}
+}
