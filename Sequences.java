3b201a74b03b3a6ace76c93681786fd0bae524d0
login <9moyapochta9@gmail.com>
2019-06-15 21:00:07 +0300

    add Sequences

---
+++ Sequences.java
@@ -0,0 +1,9 @@
+public class Sequences {
+    public static void main(String[] arg) {
+        int b = 1;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(b + " ");
+            b *= 2;
+        }
+    }
+}
