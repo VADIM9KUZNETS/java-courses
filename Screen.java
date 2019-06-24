ea5746c9203ada6e0df642f330b32afe90e3dd80
login <9moyapochta9@gmail.com>
2019-06-24 20:23:39 +0300

    add Screen

---
+++ Screen.java
@@ -0,0 +1,7 @@
+public class Screen {
+    public static void main(String[] arg) {
+        System.out.println("Today is Monday");
+        System.out.println("It's June");
+        System.out.println("My name is Vadim");
+    }
+    }
