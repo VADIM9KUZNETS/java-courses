37ecb2f81d59a93e5381d8e735b4f2bc040c1a31
login <9moyapochta9@gmail.com>
2019-06-24 20:52:24 +0300

    add Rectangle

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle {
+    public static void main(String[] arg) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 8; j++) {
+                System.out.print("A " + " ");
+            }
+            System.out.println();
+        }
+    }
+    }
