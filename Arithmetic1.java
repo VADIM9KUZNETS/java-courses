6a994f87044f4ecfea57947faa78fcd13b2d0eae
login <9moyapochta9@gmail.com>
2019-06-25 23:02:54 +0300

    add Arithmetic1

---
+++ Arithmetic1.java
@@ -0,0 +1,7 @@
+public class Arithmetic1 {
+    public static void main(String[] arg) {
+        int a = 2;
+        int b = 3;
+        System.out.println((a+(4*b))*(a + (-1)*(3*b))+(a*a));
+    }
+}
