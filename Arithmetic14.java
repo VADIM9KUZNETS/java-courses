10ad42a2b33e8ed5cd8cf5dc97002a8b35b216f9
login <9moyapochta9@gmail.com>
2019-06-27 21:03:14 +0300

    add Arithmetic14

---
+++ Arithmetic14.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Arithmetic14 {
+    public static void main(String[] arg) {
+        System.out.println("Enter the number:");
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        double a = sc.nextDouble();
+        double k = sc.nextDouble();
+        System.out.println(y*a/x);
+        System.out.println(x*k/a);
+    }
+}
