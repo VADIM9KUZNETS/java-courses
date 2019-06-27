f3091dd4ad78a19164480f8dbe48e0e7caff68a0
login <9moyapochta9@gmail.com>
2019-06-27 21:27:56 +0300

    ad Arithmetic16

---
+++ Arithmetic16.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Arithmetic16 {
+    public static void main(String[] arg) {
+        System.out.println("Enter the number:");
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        double z = sc.nextDouble();
+        System.out.println(x*7);
+        System.out.println(y*30);
+        System.out.println(z*360);
+    }
+}
