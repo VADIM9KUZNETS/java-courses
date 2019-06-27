6fcbcb91f6c574c5072a49229354852ace76c608
login <9moyapochta9@gmail.com>
2019-06-27 21:36:31 +0300

    add Arithmetic17

---
+++ Arithmetic17.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Arithmetic17 {
+    public static void main(String[] arg) {
+        System.out.println("Enter the number:");
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        System.out.println("x = " + y);
+        System.out.println("y = " + x);
+    }
+}
