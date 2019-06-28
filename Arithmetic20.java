0ca259ccdb28615c7dd2ad40c00176142a1f57c6
login <9moyapochta9@gmail.com>
2019-06-28 19:13:34 +0300

    add Arithmetic20

---
+++ Arithmetic20.java
@@ -0,0 +1,8 @@
+public class Arithmetic20 {
+    public static void main(String[] arg) {
+int y = 237;
+int z = 2;
+int num = 100;
+        System.out.println("x = " + (z + (-10)*num*z + 10*y));
+    }
+}
