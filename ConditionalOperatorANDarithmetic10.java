27dd2cfefa6d28d5fb61bc4654e1ce319f22b54f
login <9moyapochta9@gmail.com>
2019-07-01 23:20:54 +0300

    add ConditionalOperatorANDarithmetic10

---
+++ ConditionalOperatorANDarithmetic10.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class ConditionalOperatorANDarithmetic10 {
+    public static void main(String[] args) {
+        System.out.println("Enter the number:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num == 1) {
+            System.out.println("Winter");
+        } else if (num == 2) {
+            System.out.println("Winter");
+        } else if (num == 3) {
+            System.out.println("Spring");
+        } else if (num == 4) {
+            System.out.println("Spring");
+        } else if (num == 5) {
+            System.out.println("Spring");
+        } else if (num == 6) {
+            System.out.println("Summer");
+        } else if (num == 7) {
+            System.out.println("Summer");
+        } else if (num == 8) {
+            System.out.println("Summer");
+        } else if (num == 9) {
+            System.out.println("Autumn");
+        } else if (num == 10) {
+            System.out.println("Autumn");
+        } else if (num == 11) {
+            System.out.println("Autumn");
+        } else if (num == 12) {
+            System.out.println("Winter");
+        }
+    }
+}
