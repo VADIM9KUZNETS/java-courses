357c8f0db23a847e06ca132de7e0d8905c560e1f
9moyapochta9 <9moyapochta9@gmail.com>
2019-12-07 11:46:05 +0300

    create Stalker

--- Stalker.java
+++ Stalker.java
@@ -1,5 +1,9 @@
 public class Stalker{
 public static void main(String[] arg){
 System.out.println("Stalker...");
+int first = Integer.valueOf(arg[0]);
+int second = Integer.valueOf(arg[1]);
+int summ = first + second;
+System.out.println("Sum "+ summ);
 }
 }
